d0bfd1f10c6e25a4f70f5e588a2408886b124737
Fabrice Normandin <fabrice.normandin@gmail.com>
2019-02-23 14:22:45 -0500

    Added folder for A2, Starting A2-Q1
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

---
+++ Assignment2/vanilla_rnn.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+"Vanilla" RNN implementation in Pytorch.
+"""
+import os
+import numpy as np
+
+import torch
+import torch.nn as nn
+
+class VanillaRNN(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        #TODO:
+
+    def forward(self, x: torch.Tensor):
+        pass
+
