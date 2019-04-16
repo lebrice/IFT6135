8207d7f10c6aac13fecdae134d4c35ae82157451
Fabrice Normandin <fabrice.normandin@gmail.com>
2019-04-16 18:19:51 -0400

    Q1 almost done, just need to debug q1.4
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- Assignment3/density_estimation.py
+++ Assignment3/density_estimation.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
 """
 Created on Sat Mar 23 13:20:15 2019
 
@@ -28,50 +26,49 @@ N = lambda x: np.exp(-x**2/2.)/((2*np.pi)**0.5)
 plt.plot(f(torch.from_numpy(xx)).numpy(), d(torch.from_numpy(xx)).numpy()**(-1)*N(xx))
 plt.plot(xx, N(xx))
 
-
 ############### import the sampler ``samplers.distribution4'' 
+
 ############### train a discriminator on distribution4 and standard gaussian
 ############### estimate the density of distribution4
 
 #######--- INSERT YOUR CODE BELOW ---#######
  
+from q1 import get_optimal_discriminator
 
+to_tensor = lambda x: torch.as_tensor(x).float
 
+from samplers import distribution4, distribution3
+f_0 = distribution3(batch_size=512)
+f_1 = distribution4(batch_size=512)
 
-
-
-
-
-
-
-
-
-
-
-
-
+print("Training discriminator...")
+discriminator = get_optimal_discriminator(f_0, f_1)
+def estimate_density(xx: np.ndarray) -> np.ndarray:
+    d_x = discriminator(xx)
+    factor = d_x / (1 - d_x)
+    # f_0_x = next(distribution3(batch_size=xx.shape[0]))
+    f_0_x = xx
+    return (f_0_x * factor)
 
 ############### plotting things
 ############### (1) plot the output of your trained discriminator 
 ############### (2) plot the estimated density contrasted with the true density
 
-
-
-r = xx # evaluate xx using your discriminator; replace xx with the output
+# r = xx # evaluate xx using your discriminator; replace xx with the output
+r = discriminator(xx) # evaluate xx using your discriminator; replace xx with the output
 plt.figure(figsize=(8,4))
 plt.subplot(1,2,1)
 plt.plot(xx,r)
 plt.title(r'$D(x)$')
-
-estimate = np.ones_like(xx)*0.2 # estimate the density of distribution4 (on xx) using the discriminator; 
+# estimate = np.ones_like(xx)*0.2 # estimate the density of distribution4 (on xx) using the discriminator; 
+#                                # replace "np.ones_like(xx)*0." with your estimate
+estimate = estimate_density(xx) # estimate the density of distribution4 (on xx) using the discriminator; 
                                 # replace "np.ones_like(xx)*0." with your estimate
 plt.subplot(1,2,2)
 plt.plot(xx,estimate)
 plt.plot(f(torch.from_numpy(xx)).numpy(), d(torch.from_numpy(xx)).numpy()**(-1)*N(xx))
 plt.legend(['Estimated','True'])
 plt.title('Estimated vs True')
-
-
 
 
 
--- Assignment3/q1.py
+++ Assignment3/q1.py
@@ -4,119 +4,151 @@ import torch
 from torch import nn
 from torch.optim import SGD
 import numpy as np
+from typing import Iterable
 
-def jensen_shannon_divergence(p, q):
+
+def jensen_shannon_divergence(network: nn.Module, x: torch.Tensor, y: torch.Tensor) -> torch.FloatTensor:
+    D_theta_x = torch.sigmoid(network(x))
+    D_theta_y = torch.sigmoid(network(y))
+    log_2 = torch.as_tensor(np.log(2))
+    return log_2 + \
+        0.5 * torch.log(D_theta_x).mean() + \
+        0.5 * torch.log(1- D_theta_y).mean()
+
+
+def wasserstein_distance(network: nn.Module, x: torch.Tensor, y: torch.Tensor) -> torch.FloatTensor:
+    T_theta_x = network(x)
+    T_theta_y = network(y)
+    obj = T_theta_x.mean()
+    obj -= T_theta_y.mean()
+
+    lambda_coefficient = 10.0
+    obj -= lambda_coefficient * gradient_pernalty(network, x, y)
+    return obj
+
+
+def gradient_pernalty(model, x, y):
     """
-    TODO: Really unsure about this. Are we supposed to train a network that gives the JSD for two particular distributions?
-    or for any two distributions?
+    Inspired from "https://discuss.pytorch.org/t/gradient-penalty-with-respect-to-the-network-parameters/11944/2"
     """
-    D_theta = nn.Sequential(
-        nn.Linear(1, 512),
+    def random_interpolation(x, y):
+        a = np.random.uniform(0, 1)
+        return a * x + (1-a) * y
+
+    z = random_interpolation(x, y)
+    z.requires_grad = True
+
+    output = model(z)
+    gradients = torch.autograd.grad(
+        outputs=output,
+        inputs=z,
+        grad_outputs=torch.ones_like(output),
+        create_graph=True,
+        retain_graph=True,
+        only_inputs=True,
+    )
+    gradient = gradients[0]
+    norm_2 = gradient.norm(dim=1, p=2)
+    return ((norm_2 - 1)**2).mean()
+
+
+def maximize_objective(objective, p, q, maxsteps=5000, threshold=0.001):
+    p = to_tensors(p)
+    q = to_tensors(q)
+
+    p_0 = next(p)
+    q_0 = next(q)
+    assert p_0.shape == q_0.shape, "P and Q have to have the same shape of elements!"
+
+    network = nn.Sequential(
+        nn.Linear(p_0.shape[-1], 512),
         nn.Linear(512, 256),
         nn.Linear(256, 1),
-        nn.Sigmoid()
     )
 
     cuda = torch.cuda.is_available()
-    print("cuda?", cuda)
     if cuda:
-        D_theta = D_theta.cuda()
+        network = network.cuda()
     
-    D_theta.train()
-    D_theta.float()
-    params = D_theta.parameters()
+    network.train()
+    network.float()
+    params = network.parameters()
     optimizer = SGD(params, lr=1e-3)
 
-    def objective(D_theta: nn.Module, x: torch.Tensor, y: torch.Tensor) -> torch.FloatTensor:
-        D_theta_x = D_theta(x)
-        D_theta_y = D_theta(y)
-        log_2 = torch.as_tensor(np.log(2))
-        return log_2 + \
-            0.5 * torch.log(D_theta_x).mean() + \
-            0.5 * torch.log(1- D_theta_y).mean()
-
-    for i, x, y in zip(range(1000), p, q):
+    value: float = 0.0
+    hook = StopIfConverged(threshold=threshold)
+    for i, x, y in zip(range(maxsteps), p, q):
         # print(x)
-        loss = - objective(D_theta, x, y)
+        loss = - objective(network, x, y)
         loss.backward()
 
         optimizer.step()
         optimizer.zero_grad()
-        print(i, loss.item())
-        # mean_D_theta_x = D_theta_x.
-        # print(i, D_theta_x, D_theta_y)
+        value = - loss.item()
+        if hook(value):
+            print(f"converged at step {i}")
+            break
+    else:
+        print(f"Did not converge after {i} steps!")
 
-    D_theta.eval()
-    return D_theta
+    print(f"Steps: {i}, value: {value}")
+    return value, network
 
 
-def wasserstein_distance(p, q):
+class NumpyBuffer(object):
+    """FIFO buffer with numpy."""
+    def __init__(self, capacity: int):
+        self.x = np.zeros([capacity])
+    
+    def put(self, new_value):
+        self.x[:-1] = self.x[1:]; self.x[-1] = new_value
+
+
+class StopIfConverged(object):
     """
-    Also really unsure about this.
+    Little helper class that helps detect when we have converged and save us some time.
+
+    Parameters
+    ----------
+    patience :  int, optional. The number of values to keep in the buffer.
+    threshold : float, optional. The threshold on the total variation and the std.
+    maximizing: bool, optional. Whether we are maximizing or minimizing the objective.
+
+    Returns
+    ---------
+    converged: bool, Wether or not we have converged, as detected using the threshold and patience.
     """
-    T_theta = nn.Sequential(
-        nn.Linear(1, 512),
-        nn.Linear(512, 256),
-        nn.Linear(256, 1),
-    )
+    def __init__(self, patience=50, threshold=0.001, maximizing=True):
+        self.patience = patience
+        self.threshold = threshold
+        self.maximizing = maximizing
+        self._values = NumpyBuffer(patience)
+        self._steps = 0
 
-    cuda = torch.cuda.is_available()
-    print("cuda?", cuda)
-    if cuda:
-        T_theta = T_theta.cuda()
-    
-    T_theta.train()
-    T_theta.float()
-    params = T_theta.parameters()
-    optimizer = SGD(params, lr=1e-3)
-    
-    
+    def __call__(self, value):
+        self._values.put(value)
+        
+        if self._steps < self.patience:
+            self._steps += 1
+            # we haven't filled the buffer, hence we have not converged.
+            return False
 
-    def gradient_pernalty(model, x, y, lambda_coefficient):
-        """
-        Inspired from "https://discuss.pytorch.org/t/gradient-penalty-with-respect-to-the-network-parameters/11944/2"
-        """
-        def random_interpolation(x, y):
-            a = np.random.uniform(0, 1)
-            return a * x + (1-a) * y
+        total_diff = np.sum(np.diff(self._values.x))
+        if self.maximizing:
+            # if the objective is still increasing, then we have not converged.
+            if total_diff >= self.threshold:
+                return False
+        else:
+            # if the objective is still decreasing, then we have not converged.
+            if total_diff <= self.threshold:
+                return False
 
-        z = random_interpolation(x, y)
-        z.requires_grad = True
+        # If we have stagnated, then we have converged.
+        std = np.std(self._values.x)
+        return std <= self.threshold
+            
+        
 
-        output = model(z)
-        gradients = torch.autograd.grad(
-            outputs=output,
-            inputs=z,
-            grad_outputs=torch.ones_like(output),
-            create_graph=True,
-            retain_graph=True,
-            only_inputs=True,
-        )
-        gradient = gradients[0]
-        norm_2 = gradient.norm(dim=1, p=2)
-        return lambda_coefficient * ((norm_2 - 1)**2).mean()
-
-    def objective(T_theta: nn.Module, x: torch.Tensor, y: torch.Tensor, lambda_coefficient) -> torch.FloatTensor:
-        T_theta_x = T_theta(x)
-        T_theta_y = T_theta(y)
-        obj = T_theta_x.mean() - T_theta_y.mean()
-        obj -= gradient_pernalty(T_theta, x, y, lambda_coefficient)
-        return obj 
-       
-    for i, x, y in zip(range(1000), p, q):
-        lambda_coefficient = 10.0
-        loss = - objective(T_theta, x, y, lambda_coefficient)
-        loss.backward()
-
-        optimizer.step()
-        optimizer.zero_grad()
-        print(i, loss.item())
-        # break
-        # mean_D_theta_x = D_theta_x.
-        # print(i, D_theta_x, D_theta_y)
-
-    T_theta.eval()
-    return T_theta
 
 def gaussian_distribution(mean=0, std=1, mini_batch_size=512) -> np.ndarray:
     """
@@ -127,10 +159,79 @@ def gaussian_distribution(mean=0, std=1, mini_batch_size=512) -> np.ndarray:
         x = torch.as_tensor(x).float()
         yield x
 
+
+def to_tensors(generator: Iterable[np.ndarray]) -> Iterable[torch.Tensor]:
+    for item in generator:
+        yield torch.as_tensor(item).float()
+
+
+def q1(p, q):
+    return maximize_objective(jensen_shannon_divergence, p, q)
+
+
+def q2(p, q):
+    return maximize_objective(wasserstein_distance, p, q)
+
+
+def q3():
+    from samplers import distribution1
+    import matplotlib.pyplot as plt
+    
+    def get_samples(phi: float):
+        p = distribution1(0)
+        q = distribution1(phi)
+        jsd, _ = q1(p, q)
+        wd, _ = q2(p, q)
+        return jsd, wd
+
+    phis = np.arange(-1, 1.1, 0.1)
+    jsds = []; wds = []
+    for i, phi in enumerate(phis):
+        print(f"phi: {phi:.2f}")
+        jsd, wd = get_samples(phi)
+        jsds.append(jsd)
+        wds.append(wd)
+
+    fig: plt.Figure = plt.figure()
+    ax: plt.Axes = fig.add_subplot(1, 1, 1)
+
+    ax.scatter(phis, jsds, label="JSD")
+    ax.scatter(phis, wds, label="WD")
+    ax.set_title("WD and JSD vs phi")
+    ax.legend()
+    ax.set_xlabel("phi")
+    ax.set_ylabel("Distance metric value")
+    plt.show()
+    plt.savefig("./q1_3.png")
+
+
+def get_optimal_discriminator(f_0, f_1):
+    def discriminator_value_fn(network: nn.Module, x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
+        D_theta_x = nn.functional.logsigmoid(network(x))
+        D_theta_y = torch.log(1 - torch.sigmoid(network(y)))
+        return (D_theta_x + D_theta_y).mean()
+
+    value, discriminator = maximize_objective(
+        discriminator_value_fn,
+        p=f_0,
+        q=f_1,
+        maxsteps=10000,
+        threshold=0.001,
+    )
+    def disc(x: torch.Tensor) -> torch.Tensor:
+        with torch.no_grad():
+            x = torch.as_tensor(x).view([-1, 1]).float()
+            d_x = torch.sigmoid(discriminator(x))
+            return d_x.numpy()
+
+    return disc
+
+def q4():
+    import matplotlib.pyplot as plt
+    import density_estimation
+    plt.show()
+    plt.savefig("./q1_4.png")
+
 if __name__ == "__main__":
-    # from samplers import distribution1, distribution2
-    p = gaussian_distribution(0, 1)
-    q = gaussian_distribution(5, 1)
-    # div_function = jensen_shannon_divergence(p, q)
-    div_function = wasserstein_distance(p, q)
+    q4()
     
